192af0949045ec6c32cb56a434c8694fccf0ab9e
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2022-09-23 12:27:47 +0100

    Some style changes

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const style = {
   //former color:bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]
   //fmr container color:bg-slate-100
   //fmr button color : bg-purple-500
-  bg: ` h-screen w-full p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0] `,
+  bg: ` h-screen w-full p-4 bg-purple-500 `,
   container: `bg-slate-100 max-w-[500px] w-full m-auto shadow-xl p-4`,
   heading: `text-3xl font-bold text-center text-gray-800 p-2`,
   form: `flex justify-between`,
